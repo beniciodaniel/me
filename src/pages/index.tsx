9ea4dcf91c59b3eb525379c17d300712212841a5
Benício Barreto <benicio.daniel@gmail.com>
2020-11-18 15:26:19 -0300

    Criou a aba blog nos links para poder configurar a página no formato de blog. Foi utilizado a biblioteca do Prismic javascript para fazer as query na api e prismic dom para apresentar na tela os dados recebidos bem como criou-se componentes estilizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,53 @@
+import { useState } from "react";
+import { GetServerSideProps } from "next";
+import { format, parseISO } from "date-fns";
+
 import SEO from "../components/SEO";
+
+import { client } from "@/lib/prismic";
+import { Document } from "prismic-javascript/types/documents";
+import Prismic from "prismic-javascript";
+import PrismicDOM from "prismic-dom";
 
 import {
   Container,
   Background,
   ContentFooterWrapper,
   Content,
+  PostListContainer,
+  Post,
+  PostButton,
+  PostText,
+  PostImage,
   Footer,
 } from "../styles/pages/Home";
 
-export default function Home() {
+interface IHomeProps {
+  posts: Document[];
+}
+
+export default function Home({ posts }: IHomeProps) {
+  const [isPostConfiguration, setIsPostConfiguration] = useState(false);
+  const [showPostId, setShowPostId] = useState("");
+
+  function handlePostConfiguration() {
+    setIsPostConfiguration(!isPostConfiguration);
+  }
+
+  function handleClickedPostVisualization(postId: string) {
+    postId === showPostId
+      ? setShowPostId(() => "")
+      : setShowPostId(() => postId);
+  }
+
   return (
     <Container>
       <SEO title="Benício Daniel" />
 
-      <Background />
+      <Background isPostConfiguration={!!isPostConfiguration} />
 
-      <ContentFooterWrapper>
-        <Content>
+      <ContentFooterWrapper isPostConfiguration={!!isPostConfiguration}>
+        <Content isPostConfiguration={!!isPostConfiguration}>
           <h3>Benício Daniel Hasegawa</h3>
           <p>developer & architect</p>
           <p>benicio.daniel@gmail.com</p>
@@ -28,23 +59,70 @@ export default function Home() {
               rel="noopener noreferrer"
               target="_blank"
             >
-              <strong>github </strong>
+              <strong>github</strong>
             </a>
             <a
               href="https://www.linkedin.com/in/benicio-daniel/"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <strong>linkedIn </strong>
+              <strong>linkedIn</strong>
             </a>
             <a
               href="https://www.instagram.com/beniciohasegawa"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <strong>instagram </strong>
+              <strong>instagram</strong>
             </a>
+            <button type="button" onClick={() => handlePostConfiguration()}>
+              <strong>blog</strong>
+            </button>
           </div>
+
+          {isPostConfiguration && !!posts.length && (
+            <PostListContainer>
+              <ul>
+                {posts.map((post) => {
+                  return (
+                    <Post key={post.id}>
+                      <PostButton
+                        onClick={() => handleClickedPostVisualization(post.id)}
+                        postId={post.id}
+                        showPostId={showPostId}
+                      >
+                        <span>
+                          {format(parseISO(post.data.postdate), "dd/MM/yyyy")}
+                        </span>
+                        <strong>
+                          {PrismicDOM.RichText.asText(post.data.title)}
+                        </strong>
+                      </PostButton>
+
+                      <PostText postId={post.id} showPostId={showPostId}>
+                        <span>
+                          {PrismicDOM.RichText.asText(post.data.text)}
+                        </span>
+                      </PostText>
+
+                      <PostImage
+                        postId={post.id}
+                        showPostId={showPostId}
+                        src={post.data.thumbnail.list320.url}
+                        alt="Imagem"
+                      />
+                    </Post>
+                  );
+                })}
+              </ul>
+            </PostListContainer>
+          )}
+
+          {isPostConfiguration && !posts.length && (
+            <div>
+              <p>no posts yet :(</p>
+            </div>
+          )}
         </Content>
 
         <Footer>
@@ -60,3 +138,15 @@ export default function Home() {
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const posts = await client().query([
+    Prismic.Predicates.at("document.type", "post"),
+  ]);
+
+  return {
+    props: {
+      posts: posts.results,
+    },
+  };
+};
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,18 +1,35 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface IPostConfigurationProps {
+  isPostConfiguration: boolean;
+}
+
+interface IPostSelectedProps {
+  postId?: string;
+  showPostId?: string;
+}
 
 export const Container = styled.div`
   height: 100vh;
   display: flex;
 `;
 
-export const Background = styled.div`
+export const Background = styled.div<IPostConfigurationProps>`
   flex: 1;
   background: url("../../background.jpeg") no-repeat;
   background-size: cover;
   background-position: top;
+
+  transition: flex 0.3s;
+
+  ${(props) =>
+    props.isPostConfiguration &&
+    css`
+      flex: none;
+    `}
 `;
 
-export const ContentFooterWrapper = styled.div`
+export const ContentFooterWrapper = styled.div<IPostConfigurationProps>`
   width: 100%; // responsável pelo Content ocupar tudo quando tela diminuída
   max-width: 500px;
 
@@ -20,20 +37,41 @@ export const ContentFooterWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  transition: max-width 0.3s;
+
+  ${(props) =>
+    props.isPostConfiguration &&
+    css`
+      max-width: 100%; // se não edita isso, ficará no máximo 500
+    `}
 `;
 
-export const Content = styled.main`
+export const Content = styled.main<IPostConfigurationProps>`
+  padding: 20px 0 20px 0;
+
   width: 320px;
+  overflow: auto;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  div:last-child {
+    margin-top: 1rem;
+    display: flex;
+    justify-content: center;
+  }
 
   p + p {
     margin-bottom: 16px;
   }
 
   div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
     a {
       text-decoration: none;
       color: #232129;
@@ -48,10 +86,130 @@ export const Content = styled.main`
       }
     }
 
-    a + a {
+    a + a,
+    a + button {
       margin-left: 12px;
     }
+
+    > button {
+      border: 0;
+      background: transparent;
+
+      color: #232129;
+      padding: 5px;
+
+      font-size: 16px;
+
+      &:hover {
+        color: #d830a8;
+      }
+
+      strong {
+        text-align: center;
+      }
+
+      ${(props) =>
+        props.isPostConfiguration &&
+        css`
+          color: #d830a8;
+        `}
+    }
   }
+`;
+
+export const PostListContainer = styled.div`
+  border-top: 1px solid #232129;
+  padding-top: 16px;
+  margin-top: 1rem;
+
+  font-family: "Times New Roman", Times, serif;
+
+  ul {
+    list-style-type: none;
+    width: 100%;
+
+    li + li {
+      margin-top: 1rem;
+    }
+  }
+`;
+
+export const Post = styled.li<IPostSelectedProps>`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+export const PostButton = styled.button<IPostSelectedProps>`
+  border: 0;
+  background: transparent;
+
+  color: #232129;
+  padding: 5px;
+
+  font-size: 16px;
+  text-transform: lowercase;
+
+  padding: 0 0 5px 0;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  color: #232129;
+
+  span {
+    width: 75px; // marcando a largura exata
+    text-align: left;
+  }
+
+  &:hover {
+    color: #d830a8;
+  }
+
+  ${(props) =>
+    props.postId === props.showPostId &&
+    css`
+      color: #d830a8;
+    `}
+`;
+
+export const PostText = styled.div<IPostSelectedProps>`
+  margin-bottom: 1rem;
+
+  span {
+    text-align: justify;
+
+    transition: opacity 0.7s ease-out;
+    opacity: 0;
+    height: 0;
+    overflow: hidden;
+
+    ${(props) =>
+      props.postId === props.showPostId &&
+      css`
+        opacity: 1;
+        height: auto;
+      `}
+  }
+`;
+
+export const PostImage = styled.img<IPostSelectedProps>`
+  width: 100%;
+  height: 320px;
+
+  transition: opacity 0.7s ease-out;
+  opacity: 0;
+  height: 0;
+  overflow: hidden;
+
+  ${(props) =>
+    props.postId === props.showPostId &&
+    css`
+      opacity: 1;
+      height: auto;
+    `}
 `;
 
 export const Footer = styled.footer`
